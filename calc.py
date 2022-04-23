9955329606b83110517edc1f608090d645b91610
7DREZZ <99985897+7DREZZ@users.noreply.github.com>
2022-04-23 13:13:47 +0300

    first commit

---
+++ calc.py
@@ -0,0 +1,40 @@
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a / b
+def sqrt(a):
+    return a ** 0.5
+def pow(a, b):
+    return a ** b 
+def compare1(a, b):
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    elif b > a:
+        return -1
+def compare2(a, b):
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    elif b > a:
+        return -1
+def compare3(a, b):
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    elif b > a:
+        return -1
+def compare4(a, b):
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    elif b > a:
+        return -1
